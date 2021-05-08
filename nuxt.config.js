f48c79ddc6133c0c57eda38f9620f1f771411e52
louise <x79330@gmail.com>
2021-05-08 19:29:12 +0900

    文字調整

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/Plugins/aos.js', mode: 'client' },
+    { src: '~/plugins/aos.js', mode: 'client' },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
